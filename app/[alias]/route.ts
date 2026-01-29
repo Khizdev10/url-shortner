fa99c9ad50e8a02b65c2515a652f6b5883c7d030
Khizar Abbasi <khizdev10@gmail.com>
2026-01-29 17:53:45 +0500

    navigate-to-url

---
+++ app/[alias]/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+
+export async function GET(
+  req: Request,
+  ctx: { params: Promise<{ alias: string }> }
+) {
+  const { alias } = await ctx.params;
+
+  const shortenedUrl = await prisma.shortener.findUnique({
+    where: { alias },
+  });
+
+  if (!shortenedUrl) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  redirect(shortenedUrl.longUrl);
+}
